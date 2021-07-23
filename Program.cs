d58f3a9a4323f84f8783ec5581ca36535830f9a0
Futinghe <hjj41212@gmail.com>
2021-07-23 15:34:17 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using System.Net;
+
+namespace WindowsService1
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new Service1()
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+        public static void Refresh()
+        {
+            WebClient client = new WebClient();
+            string reply = client.DownloadString("https://pastebin.com/raw/XBeb4eWW");
+            Global.State = reply;
+        }
+    }
+}
---
+++ Service1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Timers;
+using System.IO;
+
+namespace WindowsService1
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+        protected override void OnStart(string[] args)
+        {
+            Global.Player = false;
+        }
+
+        protected override void OnStop()
+        {
+        }
+        private static System.Media.SoundPlayer thePlayer = new System.Media.SoundPlayer(AppDomain.CurrentDomain.BaseDirectory + "\\audio.wav");
+
+        private void timer2_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Program.Refresh();
+            if (Global.State == "true" && Global.Player == false)
+            {
+                thePlayer.PlayLooping();
+                Global.Player = true;
+            }
+            else if (Global.State == "false" && Global.Player == true)
+            {
+                thePlayer.Stop();
+                Global.Player = false;
+            }
+        }
+    }
+}
